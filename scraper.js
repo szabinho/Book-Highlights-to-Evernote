fecc5b83a5f57e28b45ddfab7fe6162122355fcf
Szabolcs Petho <petho.szabi@gmail.com>
2019-08-13 17:24:43 +0100

    start initial iBooks scraper

--- scraper.js
+++ scraper.js
@@ -275,6 +275,226 @@ const sp_KindleScraper = (function(){
     }
 })();
 
+const sp_IbookScraper = (function(){
+    
+    var scrapedHighlights,
+        scrapedHighlightsArray,
+        cleanScrapedHighlights,
+        sectionsWithHighlights,
+        metaSection,
+        docJSON = {
+            "authors": undefined,
+            "title": undefined,
+            "sections": []
+        },
+        sectionIndexes = [];
+    
+    /**
+     * 
+     * @param {*} docToScrape document element from the html export of Kindle highlights
+     * 
+     * @return               {
+     *                          "authors": String,
+     *                          "title": String,
+     *                          "sections": [
+     *                               {
+     *                                   "sectionTitle": String,
+     *                                   "noteCount": Number,
+     *                                   "notes": [
+     *                                       {
+     *                                           "type": String,
+     *                                           "location": String,
+     *                                           "level": String,
+     *                                           "text": String
+     *                                       }
+     *                                   ]  
+     *                               }
+     *                           ]
+     *                      }
+     */
+    function getHighlights(docToScrape) {
+        /** 
+         * NodeLists are array-like but don't feature many of the methods provided by the Array, 
+         * like forEach, map, filter, etc.
+         * 
+         * a very simple way to convert NodeLists to Arrays:
+         *   
+         *   var nodesArray = Array.prototype.slice.call(document.querySelectorAll("div"));
+         * 
+         * https://davidwalsh.name/nodelist-array
+         */ 
+        scrapedHighlights = docToScrape.querySelector('body div').childNodes;
+        scrapedHighlightsArray = Array.prototype.slice.call(scrapedHighlights); // a very simple way to convert NodeLists to Arrays
+        cleanScrapedHighlights = sp_Util.removeWhiteSpace(scrapedHighlightsArray);
+        cleanScrapedHighlights = removeSeparators(cleanScrapedHighlights);
+        sectionIndexes = getSectionIndexes(cleanScrapedHighlights);
+        sectionsWithHighlights = splitIntoSections(cleanScrapedHighlights, sectionIndexes);
+        metaSection = getScrapedMetaArray(cleanScrapedHighlights, sectionIndexes);
+
+        docJSON.sections = buildSectionJSON(sectionsWithHighlights);
+        docJSON.authors = getAuthor(metaSection);
+        docJSON.title = getTitle(metaSection);
+
+        return docJSON;
+    }
+
+    function removeSeparators(nodeArray) {
+        return nodeArray.filter(nod => !nod.classList.contains('separator'));
+    }
+
+    /**
+     * Find sections in the scraped document node list
+     * 
+     * @param nodeArray An array of cleaned nodes/elements of scraped Highlights
+     * 
+     * @return          Array of indexes of section heading in the nodeArray
+     * 
+     */
+    function getSectionIndexes(nodeArray) {
+        let indexes = [];
+        
+        nodeArray.forEach((el, index) => {
+            if (el.classList.contains('sectionHeading')) indexes.push(index);
+        });
+
+        return indexes;
+    }
+
+    /**
+     * Split the scraped document node list into sections and highlights.
+     * Leave out elements from the node list before the first section
+     * 
+     * Assumption: anything before the first sectionHeading is just meta, without highlights 
+     * 
+     * @param  nodeArray        an Array of nodeList to be split into sections
+     * @param  splitIndexes     an Array of indexes where the nodeArray is sliced to sections
+     * 
+     * @return Array of nodeLists of sections and their highlights
+     *          ex.: [div.sectionHeading...,div.noteHeading...,div.noteText...]
+     */
+    function splitIntoSections(nodeArray, splitIndexes) {
+        /**
+         * Array.prototype.slice()
+         * copy a portion of an array into a new array object selected 
+         * from begin to end (end not included) 
+         * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+         */
+        
+        var sections = [],
+            temp;
+
+        splitIndexes.forEach((splitPos, i) => {
+            var temp;
+            temp = nodeArray.slice(splitPos, splitIndexes[i+1])
+            sections.push(temp);
+        });  
+        
+        return sections;
+    }
+
+    /**
+     * Slice the begining of the nodeList until the first section (meta of the scraped Highlights)
+     * 
+     * Assumption: anything before the first sectionHeading is just meta, without highlights
+     * 
+     * @param  nodeArray        an Array of nodeList to be split into sections
+     * @param  splitIndexes     an Array of indexes where the nodeArray is sliced to sections
+     * 
+     * @return an Array of nodeList from the first element until the first section
+     */
+    function getScrapedMetaArray(nodeArray, splitIndexes) {
+        return nodeArray.slice(0,splitIndexes[0]);;
+    }
+
+    /**
+     * Builds an Array of sections with highlights
+     * 
+     * @param {Array} sections  Array of nodeList with sections and highlights,
+     *                          expects the output value of splitIntoSections function
+     * @return an Array of objects with highlights and properties =
+     *          [
+     *              {
+     *                  "sectionTitle": String,
+     *                  "noteCount": Number,
+     *                  "notes": [
+     *                     {
+     *                          "type": String,
+     *                          "location": String,
+     *                          "level": String,
+     *                          "text": String
+     *                      }
+     *                  ]  
+     *              }
+     *          ]
+     */
+    function buildSectionJSON(sections) {
+        var sectionsToJSON = [];
+
+        sections.forEach(section => {
+            var sectionJSON = {
+                "sectionTitle": undefined,
+                "noteCount": undefined,
+                "notes": []
+            };
+
+            sectionJSON.sectionTitle = section[0].innerText;
+            // First element is the title, every note is represented with 
+            // 2 elements: .noteHeading, .noteText
+            // so the number of notes are half the number of elements in a section 
+            // not counting the title
+            sectionJSON.noteCount = (section.length-1)/2; 
+
+            for (let index = 1; index < section.length; index = index+2) {
+                var note = {
+                    "type": undefined,
+                    "location": undefined,
+                    "level": undefined,
+                    "text": undefined
+                };
+                const noteHeading = section[index];
+                const noteText = section[index+1];
+
+                note.type       = noteHeading.childNodes[0].nodeValue.trim().replace('(','');
+                note.level      = noteHeading.childNodes[1].innerText.trim();
+                note.location   = noteHeading.childNodes[2].nodeValue.trim().replace(') - Location ','');
+                note.text       = noteText.innerText
+
+                sectionJSON.notes.push(note);
+            }
+            
+            sectionsToJSON.push(sectionJSON);
+
+        });
+        return sectionsToJSON;
+    }
+
+    function getAuthor(metaNodeList) {
+        var author;
+        metaNodeList.forEach(el => {
+            if (el.classList.contains('authors')) {
+                author = el.innerText;
+            }
+        });
+
+        return author;
+    }
+
+    function getTitle(metaNodeList) {
+        var title;
+        metaNodeList.forEach(el => {
+            if (el.classList.contains('bookTitle')) {
+                title = el.innerText;
+            }
+        });
+
+        return title;
+    }
+
+    return {
+        getHighlights: getHighlights
+    }
+})();
+
 const sp_Util = (function(){
     /**
      *
