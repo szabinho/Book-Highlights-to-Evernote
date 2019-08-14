d97ac81c7378b29f776abeb56ba3e13b8dbb6944
Szabolcs Petho <petho.szabi@gmail.com>
2019-08-14 22:46:36 +0100

    refactor ToC rendering

--- highlights-render.js
+++ highlights-render.js
@@ -9,14 +9,19 @@ function scrapeHighlights(sp_docToScrape) {
     } 
 
     // Templating and dom manipulation
-
-    var sp_navHTML = '';
-    var sectionHTML = '',
-    sp_sectionEl = document.createElement('article');
+    var toc__items = [],
+        sectionHTML = '',
+        sp_sectionEl = document.createElement('article');
     sp_sectionEl.classList.add('sp_highlights');
 
     reformatedJSON.sections.forEach((section, curr) => {
-        sp_navHTML += `<li id="#sp_section${curr+1}-toc">${section.sectionTitle}&nbsp;(${section.noteCount})</li>`;
+        var toc__item,
+            sectionIndex = curr+1,
+            sectionTitle = section.sectionTitle,
+            sectionNoteCount = section.noteCount;
+
+        toc__item = `<li id="#sp_section${sectionIndex}-toc">${sectionTitle}&nbsp;(${sectionNoteCount})</li>`;
+        toc__items.push(toc__item);
         
         sectionHTML += `<section class="sp_section" id="sp_section${curr+1}">`;
         sectionHTML += `<h2 class="sp_section-title">${section.sectionTitle} (${section.noteCount})</h2>`;
@@ -36,7 +41,7 @@ function scrapeHighlights(sp_docToScrape) {
     sp_header.innerHTML = '<h1 class="sp_title"></h1><p class="sp_author"></p><ol class="sp_toc"></ol>';
     sp_header.querySelector('.sp_title').innerText = reformatedJSON.title; 
     sp_header.querySelector('.sp_author').innerText = 'By: ' + reformatedJSON.authors + ' - Excerpts from: ' + reformatedJSON.source;
-    sp_header.querySelector('.sp_toc').innerHTML = sp_navHTML;
+    sp_header.querySelector('.sp_toc').innerHTML = toc__items.join('');
     sp_sectionEl.innerHTML = sectionHTML;
 
     sp_sectionEl.insertBefore(sp_header, sp_sectionEl.firstChild);
